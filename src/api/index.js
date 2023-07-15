685b30a11d2fc29cce5ef8931b0466bbd4654539
Adarsh-shakya <adarsh.shakya_cs20@gla.ac.in>
2023-07-15 18:31:32 +0530

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios' 
             // http://localhost:5000                        
-const API = axios.create({ baseURL: 'http://localhost:5000'});
+const API = axios.create({ baseURL: 'https://stackoverflowbackend.vercel.app/'});
 
 API.interceptors.request.use((req) => {
     if(localStorage.getItem('Profile')){
