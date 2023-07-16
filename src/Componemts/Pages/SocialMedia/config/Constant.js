cc0b887244600031a231f017a0c6aef76a440fe9
Adarsh-shakya <adarsh.shakya_cs20@gla.ac.in>
2023-07-16 12:09:19 +0530

    Update Constant.js

--- src/Componemts/Pages/SocialMedia/config/Constant.js
+++ src/Componemts/Pages/SocialMedia/config/Constant.js
@@ -1,3 +1,3 @@
-export const BACKEND_URL = 'https://stackoverflowbackend.vercel.app';
+export const BACKEND_URL = 'https://stackoverflowbackend-qoqn.vercel.app';
 
 //https://backend-new-git-main-adarsh-shakya.vercel.app/
